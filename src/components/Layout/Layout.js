8fe27fe9a14aed4ca45e0d9189d8deb11fecab50
CarlosGRJ <carlosgrj@outlook.com>
2021-02-06 17:56:19 -0600

    Primer commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+export const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
